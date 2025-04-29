5b96fa8d3ac61ab116ec59198e26593fd216adca
munir <smsirajulmonir@gmail.com>
2025-04-29 07:59:11 +0600

    keyof operator explored

---
+++ module1/src/2.7.ts
@@ -0,0 +1,29 @@
+{
+    // generic constraint with keyof operator
+
+    type Vehicle ={
+        bike:string;
+        car:string;
+        ship:string;
+    }
+
+    type Owner = "bike" | "car" | "ship"; //manually
+    type Owner2 = keyof Vehicle
+
+    const getPropertyValue=<X,Y extends keyof X>(obj:X, key: Y)=>{
+
+        return obj[key];
+    }
+
+    const user ={
+        name:'Mr Munir',
+        age:26,
+        address:'ctg'
+    }
+
+    const result1 = getPropertyValue(user,'name');
+
+    
+
+    // obj['key']
+}
